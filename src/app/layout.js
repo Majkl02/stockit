3b60c8dcb0f5449ad5b6d3d82cbd82ca0ba96510
AzureAD\MichalJanuška <michal.januska@nuaktiv.sk>
2025-03-14 00:11:31 +0100

    Edit of Dashboard (home page) + small changes to header component + logo added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,72 @@
-import "./globals.css";
+import './globals.css'
+import Image from 'next/image'
 
 export const metadata = {
-  title: "StockIt",
-  description: "University Final Project",
-};
+  title: 'StockIt',
+  description: 'University Final Project'
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className='flex h-screen flex-col'>
+        {/* Header */}
+        <header className='flex h-30 max-w-screen items-center justify-between bg-gray-800 px-6 py-4 text-white'>
+          <div className='flex items-center gap-5 text-5xl font-bold'>
+            <Image src='/Logo.png' alt='StockIt Logo' width={60} height={0} />
+            <h1>StockIt</h1>
+          </div>
+          <HeaderUser />
+          {/* Replace with dynamic user data */}
+        </header>
+
+        <div className='flex flex-1'>
+          {/* Sidebar Navigation */}
+          <nav className='text flex w-64 flex-col bg-gray-900 p-5 text-white uppercase'>
+            <ul className='space-y-4'>
+              <li>
+                <a href='/dashboard' className='block rounded px-4 py-2'>
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a href='/inventory' className='block rounded px-4 py-2'>
+                  Inventory
+                </a>
+              </li>
+              <li>
+                <a href='/orders' className='block rounded px-4 py-2'>
+                  Add New Item
+                </a>
+              </li>
+              <li>
+                <a href='/settings' className='block rounded px-4 py-2'>
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Main Content Area */}
+          <main className='bg-primary flex-1 overflow-auto'>{children}</main>
+        </div>
+      </body>
     </html>
-  );
+  )
+}
+
+function HeaderUser() {
+  return (
+    <div className='flex w-60 flex-col rounded-md bg-blue-800'>
+      <span className='px-4 py-2'>Michal Januska</span>
+      <div className='flex w-full gap-5'>
+        <a href='#' className='px-4 py-2'>
+          Profile
+        </a>
+        <a href='#' className='px-4 py-2'>
+          Log out
+        </a>
+      </div>
+    </div>
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,38 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
-export default function App() {
+export default function Dashboard() {
+  const [time, setTime] = useState(new Date())
+
+  // Effect, that displays current date (runs only on mount)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000) // Updates every second
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
-    <div>
-      <h1>INITIAL NEXT.JS SETUP</h1>
-    </div>
-  );
+    <>
+      <div className='flex justify-between p-10'>
+        <div>
+          <h1 className='text-7xl'>Welcome, Michal!</h1>
+          <p className='text-2xl'>Role: Superadmin</p>
+        </div>
+        <div>
+          <span className='text-2xl'>{time.toLocaleString()}</span>
+        </div>
+      </div>
+      <div className='m-10 rounded-md bg-gray-300'>
+        <h2 className='p-4 text-3xl'>Current statistics:</h2>
+        <p className='p-4 text-2xl'>Number of groups: 8</p>
+        <p className='p-4 text-2xl'>Number of rooms: 25</p>
+        <p className='p-4 text-2xl'>Number of items stocked: 362</p>
+      </div>
+    </>
+  )
 }
