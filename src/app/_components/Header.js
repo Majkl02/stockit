a50a45496209761c51fa3bcbbe79468f73448f3e
Michal Januška <michal.januska2002@gmail.com>
2025-04-18 23:59:05 +0200

    Added lucide-react + Logo changes

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import HeaderUser from './HeaderUser'
 import { useAuth } from '../context/AuthContext'
+import logo from '/public/Logo.png'
 
 export default function Header() {
   const { user } = useAuth()
@@ -10,7 +11,7 @@ export default function Header() {
   return (
     <header className='flex h-30 max-w-screen items-center justify-between bg-gray-800 px-6 py-4 text-white'>
       <div className='flex items-center gap-5 text-5xl font-bold'>
-        <Image src='/Logo.png' alt='StockIt Logo' width={60} height={0} />
+        <Image src={logo} alt='StockIt Logo' width={60} />
         <h1>StockIT</h1>
       </div>
       {user && <HeaderUser />}
